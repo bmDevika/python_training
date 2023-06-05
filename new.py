83f15bbecd4f6598640b2d304e66600665c385f8
Devika <bmdevika@gmail.com>
2023-06-05 19:23:56 +0530

    first python prgm

---
+++ new.py
@@ -0,0 +1,4 @@
+print ("Welcome to Clabs python training")
+city = input("Enter the name of your city: ")
+pet = input("Enter the name of your pet: ")
+print ("Your company name will be ", city+pet)
